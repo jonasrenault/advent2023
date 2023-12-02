8911381049af60d2c146712028935900a10ef755
Jonas Renault <jonasrenault@gmail.com>
2023-12-02 23:07:58 +0100

    day02 part 2

--- advent2023/day02.py
+++ advent2023/day02.py
@@ -1,34 +1,54 @@
 import re
 from utils.utils import Advent
-from itertools import chain
+import math
 
 advent = Advent(2)
 
-game = {"red": 12, "green": 13, "blue": 14}
+game_max = {"red": 12, "green": 13, "blue": 14}
 
 
-def parse_game(line: str):
-    id = re.search("Game (\d+)", line).group(1)
-    draws = line[line.index(":") + 1 :].split(";")
-    impossible = [is_impossible(d) for d in draws]
-    return id, any(impossible)
+def is_game_impossible(draws: list[str]):
+    for d in draws:
+        for c in d.split(","):
+            s = re.match("\s*(\d+) (\w+)\s*", c)
+            count = int(s.group(1))
+            color = s.group(2)
+            if count > game_max[color]:
+                return True
+    return False
 
 
-def is_impossible(draw: str):
-    counts = draw.split(",")
-    for c in counts:
-        for k, v in game.items():
-            if k in c:
-                count = int(c.replace(k, "").strip())
-                if count > v:
-                    return True
+def min_possible_count(draws: list[str]):
+    counts = {"red": 0, "green": 0, "blue": 0}
+    for d in draws:
+        for c in d.split(","):
+            s = re.match("\s*(\d+) (\w+)\s*", c)
+            count = int(s.group(1))
+            color = s.group(2)
+            if counts[color] < count:
+                counts[color] = count
+    return counts
 
 
 def main():
-    lines = advent.get_input_lines()
-    games = [parse_game(l) for l in lines]
-    impossible_games = [int(id) for (id, imp) in games if not imp]
-    advent.submit(1, sum(impossible_games))
+    games = advent.get_input_lines()
+    impossible_count = 0
+    for game in games:
+        id = re.search("Game (\d+)", game).group(1)
+        draws = game[game.index(":") + 1 :].split(";")
+        if not is_game_impossible(draws):
+            impossible_count += int(id)
+
+    advent.submit(1, impossible_count)
+
+    power_sum = 0
+    for game in games:
+        draws = game[game.index(":") + 1 :].split(";")
+        counts = min_possible_count(draws)
+        power = math.prod(counts.values())
+        power_sum += power
+
+    advent.submit(2, power_sum)
 
 
 if __name__ == "__main__":
