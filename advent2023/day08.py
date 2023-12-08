f0c24f5854f19a3a0c20fa5ad6c916ad3ec5bd95
Jonas Renault <jonasrenault@gmail.com>
2023-12-08 10:57:00 +0100

    Refactor day08

--- advent2023/day08.py
+++ advent2023/day08.py
@@ -6,6 +6,8 @@ from utils.utils import Advent
 
 advent = Advent(8)
 
+INSTRUCTION_KEYS = {"L": 0, "R": 1}
+
 
 def main():
     lines = advent.get_input_lines()
@@ -17,12 +19,12 @@ def main():
             values = tuple(values.replace(")", "").replace("(", "").split(","))
             nodes[key.strip()] = tuple([v.strip() for v in values])
 
-    steps = reach_end(nodes, "AAA", instructions, lambda x: x == "ZZZ")
+    steps = reach_end(nodes, instructions, "AAA", lambda x: x == "ZZZ")
     advent.submit(1, steps)
 
     starts = {key for key in nodes.keys() if key[-1] == "A"}
     steps = {
-        key: reach_end(nodes, key, instructions, lambda x: x[-1] == "Z")
+        key: reach_end(nodes, instructions, key, lambda x: x[-1] == "Z")
         for key in starts
     }
     advent.submit(2, reduce(lcm, steps.values()))
@@ -34,21 +36,17 @@ def lcm(a, b):
 
 def reach_end(
     nodes: dict[str, tuple[str, str]],
-    start: str,
     instructions: str,
+    start: str,
     stop: Callable[[str], bool],
 ):
     current = start
     idx = 0
     while not stop(current):
         instruction = instructions[idx % len(instructions)]
-        current = next_node(nodes, instruction, current)
+        current = nodes[current][INSTRUCTION_KEYS[instruction]]
         idx += 1
     return idx
-
-
-def next_node(nodes: dict[str, tuple[str, str]], instruction: str, node: str):
-    return nodes[node][0] if instruction == "L" else nodes[node][1]
 
 
 if __name__ == "__main__":
