6c4eb93b037d9eec5bd853f7fef917aa239a52c4
Jonas Renault <jonasrenault@gmail.com>
2023-12-14 14:39:11 +0100

    day14 part2

--- advent2023/day14.py
+++ advent2023/day14.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numpy.typing as npt
+from tqdm import tqdm
 from utils.utils import Advent
 
 advent = Advent(14)
@@ -8,59 +9,136 @@ advent = Advent(14)
 def main():
     lines = advent.get_input_lines()
     platform = np.array([[c for c in l] for l in lines], dtype=str)
-    # tilt(platform)
-    # advent.submit(1, get_load(platform))
-    print(platform)
-    fixed = np.where(platform == "#")
-    tilt(platform, fixed, "N")
-    print(platform)
+    tiltN(platform)
+    advent.submit(1, get_load(platform))
+
+    platform = np.array([[c for c in l] for l in lines], dtype=str)
+    cycle_to(1000000000, platform)
+    advent.submit(2, get_load(platform))
 
 
-def get_load(platform: npt.NDArray[np.str_]):
-    sx, sy = platform.shape
+def cycle_to(limit: int, platform: npt.NDArray[np.str_]):
+    """
+    Cycle to limit, finding a loop to skip steps.
+
+    Args:
+        limit (int): the limit
+        platform (npt.NDArray[np.str_]): the platform
+    """
+    cache = {}
+    start = None
+    end = None
+    for c in tqdm(range(limit)):
+        # cycle
+        tiltN(platform)
+        tiltW(platform)
+        tiltS(platform)
+        tiltE(platform)
+        # get indices of round rocks
+        rocks = np.where(platform == "O")
+        # check if we've seen to config already
+        h = hash(tuple(r.tostring() for r in rocks))
+        if h in cache:
+            # if we found a loop, break and save start and end cycles of loop
+            start = cache[h]
+            end = c
+            break
+        cache[h] = c
+
+    # if loop was found, cycle remaining steps
+    if start is not None and end is not None:
+        remaining = (limit - start) % (end - start) - 1
+        cycle_to(remaining, platform)
+
+
+def get_load(platform: npt.NDArray[np.str_]) -> int:
+    """
+    Compute load
+
+    Args:
+        platform (npt.NDArray[np.str_]): the platform
+
+    Returns:
+        int: the load
+    """
+    sx, _ = platform.shape
     return sum([sx - x for x in np.where(platform == "O")[0]])
 
 
-# def tilt(platform: npt.NDArray[np.str_]):
-#     sx, sy = platform.shape
-#     for y in range(sy):
-#         min = 0
-#         for x in range(sx):
-#             if platform[(x, y)] == "O":
-#                 platform[(x, y)] = "."
-#                 platform[(min, y)] = "O"
-#                 min += 1
-#             elif platform[(x, y)] == "#":
-#                 min = x + 1
-#     return platform
+def tiltN(platform: npt.NDArray[np.str_]):
+    """
+    Tilt platform to the north
 
-
-def tilt(platform: npt.NDArray[np.str_], fixed, dir: str):
+    Args:
+        platform (npt.NDArray[np.str_]): the platform
+    """
     sx, sy = platform.shape
+    for y in range(sy):
+        min = 0
+        for x in range(sx):
+            if platform[(x, y)] == "O":
+                platform[(x, y)] = "."
+                platform[(min, y)] = "O"
+                min += 1
+            elif platform[(x, y)] == "#":
+                min = x + 1
 
-    # North
-    rocks = np.where(platform == "O")
-    platform[rocks] = "."
-    for y in set(rocks[1]):
-        fixed_x = fixed[0][fixed[1] == y]
-        fixed_x.sort()
-        if 0 not in fixed_x:
-            fixed_x = np.insert(fixed_x, 0, 0)
-        round = rocks[0][rocks[1] == y]
-        for x in fixed_x[::-1]:
-            c = np.sum(round >= x)
-            if c:
-                platform[x + 1 : x + 1 + c, y] = "O"
-    # for y in range(sy):
-    #     min = 0
-    #     for x in range(sx):
-    #         if platform[(x, y)] == "O":
-    #             platform[(x, y)] = "."
-    #             platform[(min, y)] = "O"
-    #             min += 1
-    #         elif platform[(x, y)] == "#":
-    #             min = x + 1
-    # return platform
+
+def tiltW(platform: npt.NDArray[np.str_]):
+    """
+    Tilt platform to the west
+
+    Args:
+        platform (npt.NDArray[np.str_]): the platform
+    """
+    sx, sy = platform.shape
+    for x in range(sx):
+        min = 0
+        for y in range(sy):
+            if platform[(x, y)] == "O":
+                platform[(x, y)] = "."
+                platform[(x, min)] = "O"
+                min += 1
+            elif platform[(x, y)] == "#":
+                min = y + 1
+
+
+def tiltS(platform: npt.NDArray[np.str_]):
+    """
+    Tilt platform to the south
+
+    Args:
+        platform (npt.NDArray[np.str_]): the platform
+    """
+    sx, sy = platform.shape
+    for y in range(sy):
+        min = sx - 1
+        for x in range(sx - 1, -1, -1):
+            if platform[(x, y)] == "O":
+                platform[(x, y)] = "."
+                platform[(min, y)] = "O"
+                min -= 1
+            elif platform[(x, y)] == "#":
+                min = x - 1
+
+
+def tiltE(platform: npt.NDArray[np.str_]):
+    """
+    Tilt platform to the east
+
+    Args:
+        platform (npt.NDArray[np.str_]): the platform
+    """
+    sx, sy = platform.shape
+    for x in range(sx):
+        min = sy - 1
+        for y in range(sy - 1, -1, -1):
+            if platform[(x, y)] == "O":
+                platform[(x, y)] = "."
+                platform[(x, min)] = "O"
+                min -= 1
+            elif platform[(x, y)] == "#":
+                min = y - 1
 
 
 if __name__ == "__main__":
