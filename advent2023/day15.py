c866638647ae5ec30ede9e7ed90949b90f412319
Jonas Renault <jonasrenault@gmail.com>
2023-12-15 10:49:23 +0100

    day15 part2

--- advent2023/day15.py
+++ advent2023/day15.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 from utils.utils import Advent
+from collections import defaultdict
 
 advent = Advent(15)
 
@@ -9,6 +10,37 @@ def main():
     steps = lines[0].split(",")
     hashes = [to_hash(s) for s in steps]
     advent.submit(1, sum(hashes))
+
+    boxes = defaultdict(list)
+    for s in tqdm(steps):
+        apply_step(s, boxes)
+    advent.submit(2, get_power(boxes))
+
+
+def get_power(boxes: defaultdict[int, list[str]]) -> int:
+    power = 0
+    for b, lenses in boxes.items():
+        power += (b + 1) * sum([(i + 1) * l[1] for i, l in enumerate(lenses)])
+    return power
+
+
+def apply_step(step: str, boxes: defaultdict[int, list[str]]):
+    if "=" in step:
+        op = "="
+    else:
+        op = "-"
+    label = step[: step.index(op)]
+    box = to_hash(label)
+
+    lenses = boxes[box]
+    lensei = [i for i, l in enumerate(lenses) if l[0] == label]
+
+    if op == "-" and len(lensei) == 1:
+        del boxes[box][lensei[0]]
+    elif op == "=" and len(lensei) == 1:
+        boxes[box][lensei[0]] = (label, int(step[step.index(op) + 1 :]))
+    elif op == "=":
+        boxes[box].append((label, int(step[step.index(op) + 1 :])))
 
 
 def to_hash(s: str) -> int:
