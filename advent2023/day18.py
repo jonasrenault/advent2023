74570c1b37f6d7afeb36343e2100a29381876240
Jonas Renault <jonasrenault@gmail.com>
2023-12-27 14:07:50 +0100

    day18 part2

--- advent2023/day18.py
+++ advent2023/day18.py
@@ -1,7 +1,5 @@
 from utils.utils import Advent
-from utils.algos import neighbors
 import numpy as np
-from collections import deque
 
 
 advent = Advent(18)
@@ -9,73 +7,78 @@ advent = Advent(18)
 
 def main():
     lines = advent.get_input_lines()
-    lines = parse_lines(lines)
-    grid = dig_trenches(lines)
-    flood_fill(grid)
-    advent.submit(1, np.count_nonzero(grid == 1))
+    moves = parse_moves(lines)
+    distance, vertices = trenches(moves)
+    advent.submit(1, int(1 + (distance / 2) + poly_area(vertices)))
+
+    moves = parse_moves(lines, True)
+    distance, vertices = trenches(moves)
+    advent.submit(2, int(1 + (distance / 2) + poly_area(vertices)))
 
 
-def print_grid(grid):
-    lines = []
-    for x in range(grid.shape[0]):
-        l = ""
-        for y in range(grid.shape[1]):
-            l += "#" if grid[(x, y)] == 1 else "."
-        lines.append(l)
-    with open("grid.txt", "w") as f:
-        f.write("\n".join(lines))
+def poly_area(vertices: list[tuple[int, int]]) -> float:
+    """
+    Compute area of polygon given its vertices using Shoelace formula.
+
+    Args:
+        vertices (list[tuple[int, int]]): polygon vertices as list of x, y coordinates
+
+    Returns:
+        float: the polygon area
+    """
+    x = np.array([x for x, _ in vertices[:-1]], dtype=int)
+    y = np.array([y for _, y in vertices[:-1]], dtype=int)
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
 
 
-def parse_lines(lines):
+def parse_moves(lines: list[str], part2: bool = False) -> list[tuple[str, int]]:
+    """
+    Parse moves
+
+    Args:
+        lines (list[str]): the input lines
+        part2 (bool, optional): whether to parse hex codes as moves. Defaults to False.
+
+    Returns:
+        list[tuple[str, int]]: list of moves (dir, distance)
+    """
     out = []
+    dirs = {"0": "R", "1": "D", "2": "L", "3": "U"}
     for line in lines:
-        dir, c, color = line.split(" ")
-        out.append((dir, int(c), color))
+        dir, distance, color = line.split(" ")
+        distance = int(distance)
+        if part2:
+            distance = int(color[2:-2], 16)
+            dir = dirs[color[-2]]
+        out.append((dir, distance))
     return out
 
 
-def flood_fill(grid):
-    xs = np.where(grid[:, 0] == 1)[0]
-    src = (xs[len(xs) // 2], 0)
+def trenches(moves: list[tuple[str, int]]) -> tuple[int, list[tuple[int, int]]]:
+    """
+    Dig trenches for given moves
 
-    queue = deque()
-    queue.append(src)
+    Args:
+        moves (list[tuple[str, int]]): the moves
 
-    grid[src] = 1
-
-    while queue:
-        node = queue.popleft()
-
-        for n in neighbors(grid, node):
-            if grid[n] == 0:
-                grid[n] = 1
-                queue.append(n)
-
-
-def dig_trenches(lines):
-    grid = np.zeros((1000, 1000))
-    x, y = 500, 500
-    grid[(x, y)] = 1
-    for dir, c, color in lines:
+    Returns:
+        tuple[int, list[tuple[int, int]]]: the distance of the trenches, and the list of its vertices
+    """
+    x, y = 0, 0
+    vertices = [(x, y)]
+    distance = 0
+    for dir, dist in moves:
         if dir == "R":
-            grid[x, y : y + c] = 1
-            y += c
+            y += dist
         elif dir == "L":
-            grid[x, y - c : y + 1] = 1
-            y -= c
+            y -= dist
         elif dir == "D":
-            grid[x : x + c, y] = 1
-            x += c
+            x += dist
         elif dir == "U":
-            grid[x - c : x + 1, y] = 1
-            x -= c
-
-    ones = np.where(grid == 1)
-    minx = ones[0].min()
-    miny = ones[1].min()
-    maxx = ones[0].max()
-    maxy = ones[1].max()
-    return grid[minx : maxx + 1, miny : maxy + 1]
+            x -= dist
+        vertices.append((x, y))
+        distance += dist
+    return distance, vertices
 
 
 if __name__ == "__main__":
