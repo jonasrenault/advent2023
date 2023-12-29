1f8584e889dd41ca22487369fb1482e2cb82013b
Jonas Renault <jonasrenault@gmail.com>
2023-12-29 09:34:05 +0100

    day20 part2

--- advent2023/day20.py
+++ advent2023/day20.py
@@ -1,9 +1,10 @@
-from utils.utils import Advent
-from collections import defaultdict, deque
-from tqdm import tqdm
-from collections.abc import Iterable
 from abc import ABC, abstractmethod
+from collections import defaultdict, deque
+from collections.abc import Iterable
+from math import lcm
 
+from tqdm import tqdm
+from utils.utils import Advent
 
 advent = Advent(20)
 
@@ -82,26 +83,63 @@ def main():
     low = 0
     high = 0
     for i in tqdm(range(1000)):
-        l, h = press_btn(modules)
+        l, h, _ = press_btn(modules)
         low += l
         high += h
-
     advent.submit(1, low * high)
 
+    # qb sends to rx, will send a low pulse when all its inputs have sent a high pulse.
+    # find loops for qb's inputs sending high pulses, then get LCM
+    loops = []
+    for input in modules["qb"].memory.keys():
+        pulse = "high"
+        c = find_loops(lines, pulse, input)
+        loops.append(c)
+    advent.submit(2, lcm(*loops))
 
-def press_btn(modules: dict[str, Module]) -> tuple[int, int]:
+
+def find_loops(
+    lines: list[str], stop_pulse: str | None = None, stop_sender: str | None = None
+) -> int:
+    """
+    Find number of button pushes required for stop_sender to send stop_pulse
+
+    Args:
+        lines (list[str]): the problem input
+        stop_pulse (str | None, optional): the pulse. Defaults to None.
+        stop_sender (str | None, optional): the sender. Defaults to None.
+
+    Returns:
+        int: number of button pushes required
+    """
+    modules = get_modules(lines)
+    c = 0
+    sent = False
+    while not sent:
+        c += 1
+        _, _, sent = press_btn(modules, stop_pulse, stop_sender)
+    return c
+
+
+def press_btn(
+    modules: dict[str, Module],
+    stop_pulse: str | None = None,
+    stop_sender: str | None = None,
+) -> tuple[int, int, bool]:
     """
     Press button module, sending a low pulse to broadcaster module and
     all processing all subsequent pulses. Returns the count of low and
-    high pulses sent.
+    high pulses sent. Function will return True as third tuple element if
+    stop_pulse is provided and was sent as a result of pushing the button.
 
     Args:
         modules (dict[str, Module]): the modules
+        stop_pulse (str | None, optional): stop pulse to look for. Defaults to None.
+        stop_sender (str | None, optional): sender. Defaults to None.
 
     Returns:
-        tuple[int, int]: the low and high pulses count
+        tuple[int, int, bool]: high and low counts
     """
-
     queue = deque()
     queue.append(("button", "low", "broadcaster"))
 
@@ -113,13 +151,17 @@ def press_btn(modules: dict[str, Module]) -> tuple[int, int]:
             high += 1
         else:
             low += 1
+
+        if stop_pulse and pulse == stop_pulse and sender == stop_sender:
+            return low, high, True
+
         if dest in modules:
             to_send = modules[dest].process(pulse, sender)
             if to_send:
                 for child, p in to_send:
                     queue.append((dest, p, child))
 
-    return low, high
+    return low, high, False
 
 
 def get_modules(lines: list[str]) -> dict[str, Module]:
