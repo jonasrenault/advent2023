9d7715139fc92366daa4d50a905b895afafa848a
Jonas Renault <jonasrenault@gmail.com>
2024-01-17 09:49:27 +0100

    day22 part2

--- advent2023/day22.py
+++ advent2023/day22.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 from utils.utils import Advent
 from itertools import product
-from collections import defaultdict
-from uuid import uuid4
+from collections import defaultdict, deque
+from string import ascii_uppercase
 
 
 advent = Advent(22)
 
 
 class Brick:
+    ids = product(ascii_uppercase, repeat=3)
+
     def __init__(self, start: tuple[int, int, int], end: tuple[int, int, int]) -> None:
         cubes = []
         for i in range(len(start)):
@@ -24,7 +26,7 @@ class Brick:
         if not cubes:
             cubes.append(start)
         self.cubes = cubes
-        self.id = str(uuid4())
+        self.id = "".join(next(Brick.ids))
 
     @property
     def min_height(self) -> int:
@@ -58,10 +60,74 @@ def main():
     bricks = get_bricks(lines)
     bricks.sort(key=lambda x: x.min_height)
     fall_bricks(bricks)
-    advent.submit(1, len(get_removable(bricks)))
+    supported_by, supports = get_supports(bricks)
+    removable = get_removable(bricks, supported_by)
+    advent.submit(1, len(removable))
+
+    to_remove = set([b.id for b in bricks]) - removable
+    total = 0
+    for b in to_remove:
+        total += len(get_falling(b, supported_by, supports)) - 1
+    advent.submit(2, total)
 
 
-def get_removable(bricks: list[Brick]) -> set[Brick]:
+def get_falling(
+    removed_brick: str, supported_by: dict[str, set[str]], supports: dict[str, set[str]]
+) -> set[str]:
+    """
+    Get the bricks that would fall if you remove given brick
+
+    Args:
+        removed_brick (str): the brick to remove
+        supported_by (dict[str, set[str]]): supported_by dict
+        supports (dict[str, set[str]]): supports dict
+
+    Returns:
+        set[str]: ids of bricks that would fall, including removed_brick
+    """
+    falling = set()
+    queue = deque([removed_brick])
+
+    while queue:
+        brick = queue.popleft()
+        falling.add(brick)
+        for supported in supports[brick]:
+            if all([b in falling for b in supported_by[supported]]):
+                queue.append(supported)
+
+    return falling
+
+
+def get_supports(
+    bricks: list[Brick],
+) -> tuple[dict[str, set[str]], dict[str, set[str]]]:
+    """
+    Get dicts of which bricks support / are supported by which other bricks
+
+    Args:
+        bricks (list[Brick]): list of bricks
+
+    Returns:
+        tuple[dict[str, set[str]], dict[str, set[str]]]: dicts of supported_by and supports
+    """
+    # for each brick, find which other bricks it rests on
+    supported_by = defaultdict(set)
+    for i in range(len(bricks)):
+        brick = bricks[i]
+        for ob in bricks[:i]:
+            if ob.supports(brick):
+                supported_by[brick.id].add(ob.id)
+
+    # invert the dict to find out for each brick which other bricks it supports
+    supports = defaultdict(set)
+    for brick, on_top in supported_by.items():
+        for ob in on_top:
+            supports[ob].add(brick)
+
+    return supported_by, supports
+
+
+def get_removable(bricks: list[Brick], supported_by: dict[str, set[str]]) -> set[Brick]:
     """
     Find bricks which can be safely removed
 
@@ -71,20 +137,12 @@ def get_removable(bricks: list[Brick]) -> set[Brick]:
     Returns:
         set[Brick]: set of bricks which can be removed
     """
-    # for each brick, find which other bricks it rests on
-    supports = defaultdict(set)
-    for i in range(len(bricks)):
-        brick = bricks[i]
-        for ob in bricks[:i]:
-            if ob.supports(brick):
-                supports[brick.id].add(ob.id)
-
     # any brick can be deleted unless it supports another brick by itself
-    deletable = set([b.id for b in bricks])
-    for values in supports.values():
+    removable = set([b.id for b in bricks])
+    for values in supported_by.values():
         if len(values) == 1:
-            deletable.discard(values.pop())
-    return deletable
+            removable.discard(values.pop())
+    return removable
 
 
 def fall_bricks(bricks: list[Brick]):
